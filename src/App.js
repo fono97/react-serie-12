b4ff7e3e2fa081da684a7ce3f002c3eba1bbd63e
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2020-06-05 18:23:44 -0400

    I learnt how to create ref instate less functional and class component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
+const MyInput = (props) => {
+  return <input type="text" ref={props.inputRef} />;
+};
+
+const FunctionCustom = () => {
+  let inputRef = null;
+  const onClick = () => {
+    inputRef.focus();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <span>My Input</span>
+        <MyInput
+          inputRef={(input) => {
+            inputRef = input;
+          }}
+        />
+        <input type="submit" value="submit" onClick={onClick} />
+      </div>
     </div>
   );
+};
+
+class App extends Component {
+  onClick = () => {
+    alert(
+      `yey: ${this.firstName.value} | ${this.lastName.value} has submmitted! :)`
+    );
+  };
+  onKeyUp = (target, e) => {
+    if (e.keyCode === 13) {
+      switch (target) {
+        case "firstName":
+          this.lastName.focus();
+          break;
+        case "lastName":
+          this.age.focus();
+          break;
+        case "age":
+          this.submit.focus();
+          break;
+        default:
+          this.firstName.focus();
+      }
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <FunctionCustom />
+        <div>
+          <span>first Name</span>
+          <input
+            ref={(input) => {
+              this.firstName = input;
+            }}
+            onKeyUp={this.onKeyUp.bind(this, "firstName")}
+            type="text"
+          />
+        </div>
+        <div>
+          <span>Last name</span>
+          <input
+            ref={(input) => {
+              this.lastName = input;
+            }}
+            onKeyUp={this.onKeyUp.bind(this, "lastName")}
+            type="text"
+          />
+        </div>
+        <div>
+          <span>Age</span>
+          <input
+            ref={(input) => {
+              this.age = input;
+            }}
+            onKeyUp={this.onKeyUp.bind(this, "age")}
+            type="text"
+          />
+        </div>
+        <div>
+          <input
+            type="submit"
+            value="submit"
+            ref={(input) => {
+              this.submit = input;
+            }}
+            onKeyUp={this.onKeyUp.bind(this, "submit")}
+            onClick={this.onClick}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
